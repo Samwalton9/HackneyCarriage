63be5e49481dda0975fbd03049469dbf9d3d326b
Sam Walton <samwalton9@gmail.com>
2024-03-06 11:41:34 +0000

    Fix reverse steering and scale turn rate with speed

--- Taxi/taxi.gd
+++ Taxi/taxi.gd
@@ -3,28 +3,32 @@ extends CharacterBody2D
 @export var max_speed : float = 80
 @export var max_reverse_speed : float  = 20
 @export var acceleration : float  = 1.5
-@export var turn_rate : float  = 0.01
+@export var base_turn_rate : float  = 0.03
 @export var deceleration : float  = 0.5
 
 var current_speed : float = 0.0
 var current_reverse_speed : float  = 0.0
+var current_turn_rate : float
+
+
+func _ready():
+	current_turn_rate = base_turn_rate
+
 
 func _physics_process(_delta):
 	var up_down = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
 	var left_right = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 
-	if left_right > 0:
-		# Wheels right
-		if current_speed >= 0.0:
+	var speed_fraction = current_speed/max_speed
+	current_turn_rate = base_turn_rate * speed_fraction
+
+	if current_speed != 0:
+		if left_right > 0:
+			# Wheels right
 			turn_right()
-		else:
+		elif left_right < 0:
+			# Wheels left
 			turn_left()
-	elif left_right < 0:
-		# Wheels left
-		if current_speed >= 0.0:
-			turn_left()
-		else:
-			turn_right()
 
 	if up_down > 0:
 		# Forwards
@@ -48,7 +52,7 @@ func _physics_process(_delta):
 
 
 func turn_left() -> void:
-	rotation -= turn_rate
+	rotation -= current_turn_rate
 
 func turn_right() -> void:
-	rotation += turn_rate
+	rotation += current_turn_rate
